afaa9c08c48926058322ce1dd14716a96b38c69e
sneeu-leeu <sa070197@gmail.com>
2022-05-15 23:18:44 +0200

    set up routing and vsion control

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  resources :bands do
-    resources :members
+
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+
+    namespace :v2 do
+      resources :bands
+      resources :members
+    end
   end
 
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
